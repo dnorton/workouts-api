3a8119dc1c994f4bc99f6a255a49c7a9ae1b6a0c
Daniel Norton <daniel@dnorton.org>
2013-12-02 23:02:24 -0500

    initial local commit

---
+++ src/main/java/com/coachcaleb/model/Workout.java
@@ -0,0 +1,142 @@
+package com.coachcaleb.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.lang.Override;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+public class Workout implements Serializable
+{
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name = "id", updatable = false, nullable = false)
+   private Long id = null;
+   @Version
+   @Column(name = "version")
+   private int version = 0;
+
+   @Column
+   private String title;
+
+   @Column
+   private long distance;
+
+   @Column
+   private long duration;
+
+   @Column
+   private String description;
+
+   public Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(final Long id)
+   {
+      this.id = id;
+   }
+
+   public int getVersion()
+   {
+      return this.version;
+   }
+
+   public void setVersion(final int version)
+   {
+      this.version = version;
+   }
+
+   @Override
+   public boolean equals(Object that)
+   {
+      if (this == that)
+      {
+         return true;
+      }
+      if (that == null)
+      {
+         return false;
+      }
+      if (getClass() != that.getClass())
+      {
+         return false;
+      }
+      if (id != null)
+      {
+         return id.equals(((Workout) that).id);
+      }
+      return super.equals(that);
+   }
+
+   @Override
+   public int hashCode()
+   {
+      if (id != null)
+      {
+         return id.hashCode();
+      }
+      return super.hashCode();
+   }
+
+   public String getTitle()
+   {
+      return this.title;
+   }
+
+   public void setTitle(final String title)
+   {
+      this.title = title;
+   }
+
+   public long getDistance()
+   {
+      return this.distance;
+   }
+
+   public void setDistance(final long distance)
+   {
+      this.distance = distance;
+   }
+
+   public long getDuration()
+   {
+      return this.duration;
+   }
+
+   public void setDuration(final long duration)
+   {
+      this.duration = duration;
+   }
+
+   public String getDescription()
+   {
+      return this.description;
+   }
+
+   public void setDescription(final String description)
+   {
+      this.description = description;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (title != null && !title.trim().isEmpty())
+         result += "title: " + title;
+      result += ", distance: " + distance;
+      result += ", duration: " + duration;
+      if (description != null && !description.trim().isEmpty())
+         result += ", description: " + description;
+      return result;
+   }
+}
---
+++ src/main/java/com/coachcaleb/rest/WorkoutEndpoint.java
@@ -0,0 +1,86 @@
+package com.coachcaleb.rest;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import com.coachcaleb.model.Workout;
+
+/**
+ * 
+ */
+@Stateless
+@Path("/workouts")
+public class WorkoutEndpoint
+{
+   @PersistenceContext(unitName = "forge-default")
+   private EntityManager em;
+
+   @POST
+   @Consumes("application/json")
+   public Response create(Workout entity)
+   {
+      em.persist(entity);
+      return Response.created(UriBuilder.fromResource(WorkoutEndpoint.class).path(String.valueOf(entity.getId())).build()).build();
+   }
+
+   @DELETE
+   @Path("/{id:[0-9][0-9]*}")
+   public Response deleteById(@PathParam("id") Long id)
+   {
+      Workout entity = em.find(Workout.class, id);
+      if (entity == null)
+      {
+         return Response.status(Status.NOT_FOUND).build();
+      }
+      em.remove(entity);
+      return Response.noContent().build();
+   }
+
+   @GET
+   @Path("/{id:[0-9][0-9]*}")
+   @Produces("application/json")
+   public Response findById(@PathParam("id") Long id)
+   {
+      TypedQuery<Workout> findByIdQuery = em.createQuery("SELECT DISTINCT w FROM Workout w WHERE w.id = :entityId ORDER BY w.id", Workout.class);
+      findByIdQuery.setParameter("entityId", id);
+      Workout entity;
+      try
+      {
+         entity = findByIdQuery.getSingleResult();
+      }
+      catch (NoResultException nre)
+      {
+         entity = null;
+      }
+      if (entity == null)
+      {
+         return Response.status(Status.NOT_FOUND).build();
+      }
+      return Response.ok(entity).build();
+   }
+
+   @GET
+   @Produces("application/json")
+   public List<Workout> listAll()
+   {
+      final List<Workout> results = em.createQuery("SELECT DISTINCT w FROM Workout w ORDER BY w.id", Workout.class).getResultList();
+      return results;
+   }
+
+   @PUT
+   @Path("/{id:[0-9][0-9]*}")
+   @Consumes("application/json")
+   public Response update(Workout entity)
+   {
+      entity = em.merge(entity);
+      return Response.noContent().build();
+   }
+}
